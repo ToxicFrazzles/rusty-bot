be3d3d51113651577a3373f95f48a3a40281e2d3
Frazer Doyle <frazzles740@gmail.com>
2024-01-23 23:14:20 +0000

    More prep for web

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ Discord bot written in Rust
 ## Usage
 Download this repository
 Copy the `.envexample` file to `.env` and modify the values to suit your requirements
-`cargo run --release --features <database>` to build and run the bot
+
+`cargo run --release -p rusty_bot` to build and run the bot
+
+`cargo run --release -p rusty_web` to build and run the web interface
--- rusty-web/src/main.rs
+++ rusty-web/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::*;
+use rocket_dyn_templates::{context, Template};
+
+
+#[get("/")]
+fn index() -> Template{
+    Template::render("index", context!{})
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    // Load .env file if it exists. Falls back to loading the variables from the actual environment
+    dotenv::dotenv().ok();
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
