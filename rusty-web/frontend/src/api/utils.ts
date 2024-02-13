eb6d60f8d6214c81d4613da940d25ec02dba8a44
toxicfrazzles <frazzles740@gmail.com>
2024-02-13 21:08:04 +0000

    More work on login stuff

--- rusty-web/frontend/src/api/utils.ts
+++ rusty-web/frontend/src/api/utils.ts
@@ -1,2 +1,7 @@
 export const API_BASE = import.meta.env.VITE_API_URL || "/api"
 
+export async function apiGet(endpoint: String){
+    console.log(API_BASE + endpoint);
+    let response = await fetch(API_BASE + endpoint);
+    return await response.json();
+}
